ff1ad4424b8fc184573861ffc05a9d569c582439
Ikbal Alissa <m99alissa@gmail.com>
2022-01-25 12:14:14 +0000

    Update object in mongodb

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ MongoClient.connect(
   }
 );
 
-
 //Get the MongoDB collection name
 app.param("collectionName", (req, res, next, collectionName) => {
   req.collection = db.collection(collectionName);
@@ -33,11 +32,32 @@ app.get("/collection/:collectionName", (req, res, next) => {
   });
 });
 
-
 // adding objects to mongodb collection
 app.post("/collection/:collectionName", (req, res, next) => {
   req.collection.insert(req.body, (e, results) => {
     if (e) return next(e);
     res.send(results.ops);
   });
+});
+
+// request a specific object using mongodb id
+const ObjectID = require("mongodb").ObjectID;
+app.get("/collection/:collectionName/:id", (req, res, next) => {
+  req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+    if (e) return next(e);
+    res.send(result);
+  });
+});
+
+// update object in mongodb
+app.put("/collection/:collectionName/:id", (req, res, next) => {
+  req.collection.update(
+    { _id: new ObjectID(req.params.id) },
+    { $set: req.body },
+    { safe: true, multi: false },
+    (e, result) => {
+      if (e) return next(e);
+      res.send(result.result.n === 1 ? { msg: "success" } : { msg: "error" });
+    }
+  );
 });
